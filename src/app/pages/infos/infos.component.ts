ad052bb18b2542f969b69aa7afe340aafa72952c
Willian Amancio <wvsamancio@gmail.com>
2023-04-22 16:09:55 -0300

    showing infos in html

--- src/app/pages/infos/infos.component.ts
+++ src/app/pages/infos/infos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Info } from 'src/app/interfaces/info';
+import { InfoService } from 'src/app/services/info.service';
 
 @Component({
   selector: 'app-infos',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./infos.component.css']
 })
 export class InfosComponent {
+  public infos: Info | any  
 
+  constructor(private infoService: InfoService) {}
+
+  ngOnInit(): void {
+    this.infoService.getInfos().subscribe(
+      response => {
+        this.infos = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
